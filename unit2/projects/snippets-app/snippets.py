d6462585ba0cb549967e38d449238a159d37cfe4
Hildner <christopher.hildner@gmail.com>
2016-08-11 03:49:40 +0000

    Add parser structure

--- unit2/projects/snippets-app/snippets.py
+++ unit2/projects/snippets-app/snippets.py
@@ -37,15 +37,24 @@ def main():
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")  
     
-    arguments = parser.parse_args()
-    
     # Subparser for the get command
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Revive a snippet")
-    get_parser.get_argument("name", help="Name of the snippet")
-    
+    get_parser.add_argument("name", help="Name of the snippet")
     
     arguments = parser.parse_args()
+    
+    #Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+    
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet,name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+        
 
 if __name__ == "__main__":
     main()
